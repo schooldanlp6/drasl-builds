da21ebc5d823e8a69e764750d3305b75041c4a3f
DanLP6 <danlp6.com@outlook.de>
2025-12-31 15:06:48 +0100

    updated gitignore for building repo

---
+++ node_modules/skinview-utils/build/load-image.d.ts
@@ -0,0 +1,7 @@
+export declare type RemoteImage = string | {
+    src: string;
+    /** @defaultvalue "anonymous" */
+    crossOrigin?: string | null;
+    referrerPolicy?: string;
+};
+export declare function loadImage(source: RemoteImage): Promise<HTMLImageElement>;
